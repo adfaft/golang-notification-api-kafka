d5db4220c2dea76cc702b1b13790b3c1662219fb
Fauzi Ezi <197737160+fauzi-ezi@users.noreply.github.com>
2025-09-16 17:13:52 +0700

    response

--- go-notification/producer/main.go
+++ go-notification/producer/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -15,14 +17,29 @@ dan push ke kafka
 **/
 
 func postMessage(c *gin.Context) {
+	var data map[string]interface{}
+	if err := json.Unmarshal([]byte(`{"test": "tersting"}`), &data); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "input is wrong"})
+		return
+	}
 
-	c.IndentedJSON(http.StatusOK, gin.H{"test": "tersting"})
+	c.IndentedJSON(http.StatusOK, gin.H{"test": "testing"})
 }
 
 func main() {
 
-	router := gin.Default()
-	router.POST("/api/message", postMessage)
+	type responseMessage struct {
+		Status  int    `json:"status"`
+		Message string `json:"message"`
+	}
+	json, _ := json.Marshal(&responseMessage{
+		Status:  200,
+		Message: "success",
+	})
+	fmt.Println(string(json))
 
-	router.Run("localhost:8001")
+	// router := gin.Default()
+	// router.POST("/api/message", postMessage)
+
+	// router.Run("localhost:8001")
 }
